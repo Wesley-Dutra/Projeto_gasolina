a7b9506984a2dfe9a2dd407dc3bfcdf88cbdd75c
Wesley Dutra Paiz <wesleydtrp19@gmail.com>
2023-09-21 13:48:04 -0300

    Formatação dos campos da tela principal

--- lib/CampoTxt.dart
+++ lib/CampoTxt.dart
@@ -17,20 +17,66 @@ class _HomeState extends State<Home> {
       ),
       body: Center(
         child: Padding(
-          padding: EdgeInsets.all(25),
+          padding: EdgeInsets.all(35),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: 'Digite um valor',
-                  labelStyle: TextStyle(
-                    color: Colors.amber
+              Padding(
+                padding: EdgeInsets.only(bottom: 25),
+                child: Image.asset(
+                  'images/gasolina.png',
+                  width: 200,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 15),
+                child: Text(
+                  'Saiba qual a melhor opção para abastecimento do seu carro:',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                   ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(width: 1, color: Colors.amber)
-                  )
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: 'Valor da Gasolina',
+                      labelStyle: TextStyle(
+                          color: Colors.amber
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(width: 1, color: Colors.amber)
+                      ),
+                      hintText: 'Ex: 5.43'
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10, bottom: 20),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: 'Valor do Álcool',
+                      labelStyle: TextStyle(
+                          color: Colors.amber
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(width: 1, color: Colors.amber)
+                      ),
+                      hintText: 'Ex: 5.43'
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('CALCULAR'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.amber,
+                  minimumSize: Size(200, 50)
                 ),
               ),
             ],
